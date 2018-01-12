d654f8c30e11cf2a766e982c0c56339b17c55118
ylu00 <ylu00@192.168.0.196>
2018-01-11 18:21:55 -0800

    PalindromePermutation

---
+++ Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/MyAnswer.java
@@ -0,0 +1,72 @@
+package Q1_04_Palindrome_Permutation;
+
+public class MyAnswer {	
+	// TODO
+	// Two issues in my implementation:
+	// 1. Need to only count the Alphabetic
+	// 2. May need to convert all to lower case as Palindrome ignore case.
+	// 3. In the solution, Character.getNumericValue() returns the same value for uppercase and lowercase character.
+	public static boolean isPermutationOfPalindrome(String phrase) {
+		if (phrase == null) {
+			return false;
+		}
+		int[] charTable = new int[128];
+		
+		char[] charPhrase = phrase.toLowerCase().toCharArray();
+		for (int i = 0; i < charPhrase.length; i++) {
+			if (Character.isAlphabetic(charPhrase[i])) {
+				charTable[charPhrase[i]]++;
+			}
+		}
+		int oddCount = 0;
+		for (int i = 0; i < charTable.length; i++) {
+			if ((charTable[i]%2) == 1) {
+				oddCount++;
+				if (oddCount > 1) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isPermutationOfPalindrome2(String phrase) {
+		if (phrase == null) {
+			return false;
+		}
+		int[] charTable = new int[26];
+		
+		char[] charPhrase = phrase.toCharArray();
+		
+		// Note: Character.getNumericValue() returns the same value for uppercase and lowercase character.
+		// range 10 to 35
+		int a = Character.getNumericValue('a');
+		int z = Character.getNumericValue('z');
+		
+		for (int i = 0; i < charPhrase.length; i++) {
+			int numValue = Character.getNumericValue(charPhrase[i]);
+			if (a <= numValue && numValue <= z) {
+				charTable[numValue-a]++;
+			}
+		}
+		int oddCount = 0;
+		for (int i = 0; i < charTable.length; i++) {
+			if ((charTable[i]%2) == 1) {
+				oddCount++;
+				if (oddCount > 1) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String pali = "Rats live on no evil star";
+		System.out.println(isPermutationOfPalindrome(pali));
+		
+		System.out.println(isPermutationOfPalindrome2(pali));
+	}
+
+
+}
