92f54b6443ecac6439fd03617b59c59624aa8fbd
yonglu <ylu007@gmail.com>
2018-01-12 19:35:37 -0800

    Finished KthToLast linkedlist

--- Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/MyAnswer.java
+++ Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/MyAnswer.java
@@ -4,40 +4,35 @@ import CtCILibrary.*;
 
 public class MyAnswer {
 
-	public static int printKthToLast(LinkedListNode head, int k) throws Exception {
+	public static LinkedListNode printKthToLast(LinkedListNode head, int k) {
       LinkedListNode runner = head;
 	   LinkedListNode lookAhead = head;
 	   for (int i = 0; i < k; i++) {
 	      if (lookAhead == null) {
-	         throw new Exception("k is out of is longer than the linked list");
+	         return null;
 	      }
 	      lookAhead = lookAhead.next;
 	   }
-	   while (lookAhead.next != null) {
+	   
+	   while (lookAhead != null) {
 	      runner = runner.next;
 	      lookAhead = lookAhead.next;
 	   }
 	   
-	   return runner.data;
+	   return runner;
 	}
 	
 	public static void main(String[] args) {
 		int[] array = {0, 1, 2, 3, 4, 5, 6};
 		LinkedListNode head = AssortedMethods.createLinkedListFromArray(array);
-      try {
-         System.out.println(0 + "th to last is: " + printKthToLast(head, 7));
-      } catch (Exception e1) {
-         // TODO Auto-generated catch block
-         e1.printStackTrace();
-      }
-
+		
 		for (int i = 0; i <= array.length + 1; i++) {
-			try {
-	         System.out.println(i + "th to last is: " + printKthToLast(head, i));
-         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         }
+			LinkedListNode tempNode = printKthToLast(head, i);
+			if (tempNode == null) {
+				System.out.println(i + "th to last is null");				
+			} else {
+				System.out.println(i + "th to last is: " + tempNode.data);
+			}
 		}
 	}
 
--- Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/QuestionD.java
+++ Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/QuestionD.java
@@ -24,7 +24,7 @@ public class QuestionD {
 	}
 	
 	public static void main(String[] args) {
-		int[] array = {0, 1, 2, 3};
+		int[] array = {0, 1, 2, 3, 4, 5, 6};
 		LinkedListNode head = AssortedMethods.createLinkedListFromArray(array);
 		for (int i = 0; i <= array.length + 1; i++) {
 			LinkedListNode node = nthToLast(head, i);
