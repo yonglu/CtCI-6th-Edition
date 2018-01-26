aa7f8259118b80476fab343a66dbe651f9b86f7c
yonglu <ylu007@gmail.com>
2018-01-25 17:13:16 -0800

    Boggle Board Word Search Using DFS

--- Java/Ch 08. Recursion and Dynamic Programming/BoogleBoardWordSearch/MyAnswer.java
+++ Java/Ch 08. Recursion and Dynamic Programming/BoogleBoardWordSearch/MyAnswer.java
@@ -21,7 +21,11 @@ Return ["eat","oath"].
 
 public class MyAnswer {	
 	
-	public static List<String> findWords(char[][] board, String[] words) {
+	private static int[][] deltas = {
+			{-1, -1}, {-1,0}, {-1, 1}, {0,-1},{0,1}, {1, -1}, {1,0}, {1, 1}
+	};
+	
+	public static List<String> findWords2(char[][] board, String[] words) {
 		ArrayList<String> result = new ArrayList<String>();
 	 
 		int m = board.length;
@@ -78,6 +82,108 @@ public class MyAnswer {
 		return false;
 	}	
 	
+	public static class Point {
+		public int row;
+		public int col;
+		Point(int r, int c) {
+			row = r;
+			col = c;
+		}
+	}
+	
+	public static boolean findWords(char[][] board, String word, int row, int col, 
+			int index, Set<Point> visited) {
+		if (row < 0 || row >= board.length || col < 0 || col >= board[0].length ||
+				index >= word.length() ) {
+			return false;
+		}
+		char c = word.charAt(index);
+		
+/*		
+ * 	See the improve block below		
+ * 
+ * 		// if it is last character, that's the end case
+		if (index == word.length() - 1) {
+			if (c == board[row][col]) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			visited.add(new Point(row, col));
+			if (c == board[row][col]) {
+				for (int[] delta : deltas) {
+					int newRow = row + delta[0];
+					int newCol = col + delta[1];
+					if (!visited.contains(new Point(newRow, newCol))) {
+						// if find our word, no need to search other paths
+						if (findWords(board, word, newRow, newCol, index+1, visited)) {
+							return true;
+						}
+					}
+				}
+			} else {
+				return false;
+			}			
+		} 
+*/		
+		visited.add(new Point(row, col));
+		if (c == board[row][col]) {
+			if (index == word.length() - 1) {
+				return true;
+			} else {
+				if (findWords(board, word, row-1, col-1, index+1, visited) ||
+						findWords(board, word, row-1, col, index+1, visited) ||
+						findWords(board, word, row-1, col+1, index+1, visited) ||
+						findWords(board, word, row, col-1, index+1, visited) ||
+						findWords(board, word, row, col+1, index+1, visited) ||
+						findWords(board, word, row+1, col-1, index+1, visited) ||
+						findWords(board, word, row+1, col, index+1, visited) ||
+						findWords(board, word, row+1, col+1, index+1, visited)) {
+					return true;
+				}
+/*				for (int[] delta : deltas) {
+					int newRow = row + delta[0];
+					int newCol = col + delta[1];
+					if (!visited.contains(new Point(newRow, newCol))) {
+						// if find our word, no need to search other paths
+						if (findWords(board, word, newRow, newCol, index+1, visited)) {
+							return true;
+						}
+					}
+				}				
+*/			}
+		} else {
+			return false;
+		}
+		
+		return false;
+	}
+	
+	public static Set<String> findWords(char[][] board, String[] words) {
+		if (board == null || words == null || 
+				(board.length == 0 && board[0].length == 0) || words.length == 0) {
+			return null;
+		}
+		// Use set instead of map so no duplicate words
+		Set<String> wordsFound = new HashSet<String>();
+		for (String word : words) {
+			char c = word.charAt(0);
+			for (int i=0; i< board.length; i++) {
+				for (int j=0; j<board[0].length; j++) {
+					if (c == board[i][j]) {
+						Set<Point> visited = new HashSet<Point>();
+						if (findWords(board, word, i, j, 0, visited)) {
+							wordsFound.add(word);
+						}						
+					}
+				}
+			}
+		}
+		
+		return wordsFound;
+	}	
+	
 	public static void main(String[] args) {
       char board[][] = new char[][] {  
     	  	{'o','a','a','n'},
@@ -90,7 +196,7 @@ public class MyAnswer {
     		  "oath","pea","eat","rain"
       };
       
-      List<String> wordsFound = findWords(board, words);
+      Set<String> wordsFound = findWords(board, words);
       for (String word : wordsFound) {
     	  System.out.println(word);
       }
