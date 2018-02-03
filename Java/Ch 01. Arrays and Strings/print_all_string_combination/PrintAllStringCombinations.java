4fd04cab2caf2861effb52ee34ca4b91c216738f
yonglu <ylu007@gmail.com>
2018-02-02 21:59:16 -0800

    Print all string combination with two 'a'.

---
+++ Java/Ch 01. Arrays and Strings/print_all_string_combination/PrintAllStringCombinations.java
@@ -0,0 +1,70 @@
+package print_all_string_combination;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PrintAllStringCombinations {
+	
+	// f(int n)
+	// generate and return all possible strings of length n
+	// containing exactly two a's
+
+	// e.g.  f(4)
+	// returns aabb, aabc, ... gaua...  zzaa
+
+	private static Set<Character> allowChars = new HashSet<Character>();
+	private static ArrayList<String> my_strings = new ArrayList<String>();
+	 
+	public static void f(int n) {
+	        
+	    for(int i = 0; i < n-1; i++) {
+	        for(int j = i+1; j < n; j++) {
+	        	char[] temp_str = new char[n];
+	        	temp_str[i] = 'a';
+	        	temp_str[j] = 'a';
+	    		boolean[] remainLoc = new boolean[n];
+	    		Arrays.fill(remainLoc, true);
+	    		remainLoc[i] = false;
+	        	remainLoc[j] = false;
+	            constructString(n, remainLoc, temp_str);                    
+	        }
+	    }
+
+	} 
+
+	public static void constructString(int n, boolean[] remainLoc, char[] temp_str) {
+	    int remain = -1;
+	    for (int k = 0; k < n; k++) {
+	    	if (remainLoc[k] == true) {
+	    		remain = k;
+	    		break;
+	    	}
+	    }
+	    
+	    if (remain == -1) {
+	        my_strings.add(new String(temp_str));
+	        return;
+	    }
+	    
+	    for (Character c: allowChars) {
+	    	temp_str[remain] = c;
+		    remainLoc[remain] = false;
+	    	constructString(n, remainLoc, temp_str); 
+	    	//TODO forgot to do the backtracking in first run.
+		    remainLoc[remain] = true;
+	    }
+	}
+	
+	public static void main(String[] args) {
+		allowChars.add('b');
+		allowChars.add('c');
+		
+		f(4);
+		for (String s : my_strings) {
+			System.out.println(s);
+		}
+	}
+
+}
