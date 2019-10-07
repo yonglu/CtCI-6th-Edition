d7c8bcbccd8573d7a45c5c9d8addc0d62172adad
Yong Lu <y.lu@f5.com>
2019-10-07 15:21:01 -0700

    Update solution

--- Java/Dynamic Programming/Coin_Change_ii_518/Coin_Change_ii_518.java
+++ Java/Dynamic Programming/Coin_Change_ii_518/Coin_Change_ii_518.java
@@ -2,7 +2,6 @@ package Coin_Change_ii_518;
 
 import java.util.*;
 
-import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
 
 /*
  * Leetcode # 518. Coin Change 2
@@ -45,21 +44,95 @@ the answer is guaranteed to fit into signed 32-bit integer
 
 
 /*
- * Note, if ask minimum steps for path, it is kind of messy to do the backtracking.  
- * Mistakes made:
- * 		* set return from coinChange to 0 if no solution, should return 
- * 				MAX_VALUE to make it easier to process (line 60)
- * 		* add one before checking if the value is MAX_VALUE. (line 77)
- * 
- * It is even harder to get the path order(e.g. 1->5->5 for 11) with memorization.
- * Need to remember the path from the backward not from beginning.  
- * 
- * During interview, just implement the backtracking algorithm without memorization first,
- * then mention that you can memorization to decrease the big O from O(2^N) to O(N^2).
+ * Note, the answer is order doesn't matter.  coinChangeNumberOfWaysBackTrackingOrderDoesntMatter 
+ * has correct answer.
  * 
  */
 public class Coin_Change_ii_518 {	
 	
+	
+	/*
+	 * Decision Tree:
+	 * 		1. order doesn't matter
+	 * 		2. item from input can be used multiple times
+	 * 		3. No need to filter duplicated
+	 */
+	public static int coinChangeNumberOfWaysBackTrackingOrderDoesntMatter(int[] coins, int amount) {
+		if (coins == null || coins.length == 0 || amount < 0) {
+			return 0;
+		} 		
+		
+		List<List<Integer>> lists = new ArrayList<>();
+		coinChangeNumberOfWaysBackTrackingOrderDoesntMatter(coins, lists, new ArrayList<Integer>(), amount, 0);
+	
+		return lists.size();
+		
+	}
+	
+	private static void coinChangeNumberOfWaysBackTrackingOrderDoesntMatter(int[] coins, 
+			List<List<Integer>> lists, ArrayList<Integer> tempList, int amount, int start) {
+		if (amount < 0) {
+			return;
+		} else if (amount == 0) {
+//			System.out.println(tempList);
+			lists.add(tempList);
+			return;
+		}
+		
+		for (int i=start; i<coins.length; i++) {
+			tempList.add(coins[i]);
+//			System.out.print(coins[i] + ", ");
+			coinChangeNumberOfWaysBackTrackingOrderDoesntMatter(coins, lists, tempList, amount-coins[i], i);
+			tempList.remove(tempList.size()-1);
+		}
+	}
+
+	/*
+	 * Decision Tree:
+	 * 		1. order matter
+	 * 		2. item from input can be used multiple times
+	 * 		3. No need to filter duplicated
+	 */
+	public static int coinChangeNumberOfWaysBackTrackingOrderMatter(int[] coins, int amount) {
+		if (coins == null || coins.length == 0 || amount < 0) {
+			return 0;
+		} 		
+		
+		List<List<Integer>> lists = new ArrayList<>();
+		coinChangeNumberOfWaysBackTrackingOrderMatter(coins, lists, new ArrayList<Integer>(), amount);
+	
+		return lists.size();
+		
+	}
+	
+	private static void coinChangeNumberOfWaysBackTrackingOrderMatter(int[] coins, 
+			List<List<Integer>> lists, ArrayList<Integer> tempList, int amount) {
+		if (amount < 0) {
+			return;
+		} else if (amount == 0) {
+//			System.out.println(tempList);
+			lists.add(tempList);
+			return;
+		}
+		
+		for (int i=0; i<coins.length; i++) {
+			tempList.add(coins[i]);
+//			System.out.print(coins[i] + ", ");
+			coinChangeNumberOfWaysBackTrackingOrderMatter(coins, lists, tempList, amount-coins[i]);
+			tempList.remove(tempList.size()-1);
+		}
+	}
+		
+	/*
+	 * This is just a slightly different implementation of the above 
+	 * coinChangeNumberOfWaysBackTrackingOrderMatter with memorization.
+	 * 
+	 * With memorization to decrease the big O from O(2^N) to O(N^2).
+	 * 
+	 * Note, if you are getting the all path combinations, it is hard to do memorization,
+	 * but if it is for # of ways or minimum items in path, then it is easier to do
+	 * memorization. 
+	 */
 	public static int coinChangeNumberOfWays(int[] coins, int amount) {
 		if (coins == null || coins.length == 0 || amount < 0) {
 			return 0;
@@ -75,15 +148,16 @@ public class Coin_Change_ii_518 {
 		if (rem < 0) {
 			return 0;
 		} else if (rem == 0) {
+//			System.out.println("Found a way");
 			return 1;
 		}
 		int result = 0;
-//		if (memo.containsKey(rem)) {
-//			return memo.get(rem);
-//		}
+		if (memo.containsKey(rem)) {
+			return memo.get(rem);
+		}
 			
 		for (int coin : coins) {
-			System.out.print(coin + ", ");
+//			System.out.print(coin + ", ");
 			result += coinChangeNumberOfWays(coins, memo, rem - coin);
 		}
 		memo.put(rem, result);
@@ -91,37 +165,9 @@ public class Coin_Change_ii_518 {
 		return result;
 	}
 
-	public static int coinChangeNumberOfWaysBackTracking(int[] coins, int amount) {
-		if (coins == null || coins.length == 0 || amount < 0) {
-			return 0;
-		} 		
-		
-		List<List<Integer>> lists = new ArrayList<>();
-		coinChangeNumberOfWaysBackTracking(coins, lists, new ArrayList<Integer>(), amount, 0);
 	
-		return lists.size();
-		
-	}
 	
-	private static void coinChangeNumberOfWaysBackTracking(int[] coins, 
-			List<List<Integer>> lists, ArrayList<Integer> tempList, int amount, int start) {
-		if (amount < 0) {
-			return;
-		} else if (amount == 0) {
-			lists.add(tempList);
-		}
-		
-		for (int i=start; i<coins.length; i++) {
-			tempList.add(coins[i]);
-//			System.out.println();
-			System.out.print(coins[i] + ", ");
-//			System.out.println();
-			coinChangeNumberOfWaysBackTracking(coins, lists, tempList, amount-coins[i], i);
-			tempList.remove(tempList.size()-1);
-		}
-	}
-	
-    public static int changeDP(int[] coins, int amount ) {
+    public static int coinChangeNUmberOfWaysIterative(int[] coins, int amount ) {
         if (amount < 0 || coins == null || coins.length == 0) {
             return amount == 0 ? 1 : 0;
         }
@@ -145,8 +191,11 @@ public class Coin_Change_ii_518 {
  
     }
     
+    /*
+     * This is doing the backtracking, but just count the ways and do not build the set of valid conins.
+     */
     public static int count=0;
-    public static int change(int[] coins, int amount) {
+    public static int coinChangeNumberOfWaysBackTrackingOrderDoesntMatterSimplified(int[] coins, int amount) {
         helper(amount, coins, 0, 0);
         return count;
     }
@@ -165,9 +214,8 @@ public class Coin_Change_ii_518 {
 
 	
 	public static void main(String[] args) {
+		int result = 0;
 		
-		int result = coinChangeNumberOfWays(new int[] { 1, 2, 3 }, 4);		
-		System.out.println("coinChange number of ways [1,2,3], 4 : " + result);
 /*
 		result = coinChangeNumberOfWays(new int[] { 1, 2, 5 }, 11);		
 		System.out.println("coinChange number of ways [1,2,5], 11 : " + result);
@@ -178,31 +226,47 @@ public class Coin_Change_ii_518 {
 		result = coinChangeNumberOfWays(new int[] { 2 }, 3);		
 		System.out.println("coinChange number of ways [2], 3 : " + result);
 */
-		result = coinChangeNumberOfWaysBackTracking(new int[] { 1, 2, 3 }, 4);		
-		System.out.println("coinChange number of ways backtracking [1,2,3], 4 : " + result);
-		
-/*		
+
+				
 		System.out.println("Test the execution times...");
 		
 		long currentTime = System.currentTimeMillis();
 		result = coinChangeNumberOfWays(new int[] { 1, 2, 5 }, 30);		
-		System.out.println("coinChange number of ways [1,2,5], 11 : " + result);
+		System.out.println("coinChange number of ways backtracking order matter with memorization [1,2,5], 11 : " + result);
 		System.out.println(System.currentTimeMillis() - currentTime);
 		
 		currentTime = System.currentTimeMillis();
-		result = coinChangeNumberOfWaysBackTracking(new int[] { 1, 2, 5 }, 30);		
-		System.out.println("coinChange number of ways backtracking [1,2,5], 11 : " + result);
-		System.out.println(System.currentTimeMillis() - currentTime);
-		
-		currentTime = System.currentTimeMillis(); 
-		result = changeDP(new int[] { 1, 2, 5 }, 30);		
-		System.out.println("coinChange number of ways DP [1,2,5], 11 : " + result);
+		result = coinChangeNumberOfWaysBackTrackingOrderMatter(new int[] { 1, 2, 5 }, 30);		
+		System.out.println("coinChange number of ways backtracking order matter [1,2,5], 11 : " + result);
 		System.out.println(System.currentTimeMillis() - currentTime);
 
 		currentTime = System.currentTimeMillis();
-		result = change(new int[] { 1, 2, 5 }, 30);		
-		System.out.println("coinChange number of ways DP [1,2,5], 11 : " + result);
+		result = coinChangeNumberOfWaysBackTrackingOrderDoesntMatter(new int[] { 1, 2, 5 }, 30);		
+		System.out.println("coinChange number of ways backtracking order doesn't matter [1,2,5], 11 : " + result);
 		System.out.println(System.currentTimeMillis() - currentTime);
-*/
+		
+		currentTime = System.currentTimeMillis(); 
+		result = coinChangeNUmberOfWaysIterative(new int[] { 1, 2, 5 }, 30);		
+		System.out.println("coinChange number of ways iterative [1,2,5], 11 : " + result);
+		System.out.println(System.currentTimeMillis() - currentTime);
+
+		currentTime = System.currentTimeMillis();
+		result = coinChangeNumberOfWaysBackTrackingOrderDoesntMatterSimplified(new int[] { 1, 2, 5 }, 30);		
+		System.out.println("coinChange number of ways backtracking order doesn't matter simplified [1,2,5], 11 : " + result);
+		System.out.println(System.currentTimeMillis() - currentTime);
+		
+/*
+ 		// Need to uncomment the System.out.println in code to show the order
+		System.out.println("Show order of execution...");
+
+		result = coinChangeNumberOfWays(new int[] { 1, 2, 3 }, 4);		
+		System.out.println("coinChange number of ways [1,2,3], 4 : " + result);
+
+		result = coinChangeNumberOfWaysBackTrackingOrderDoesntMatter(new int[] { 1, 2, 3 }, 4);		
+		System.out.println("coinChange number of ways backtrackingn order doesn't matter [1,2,3], 4 : " + result);
+		
+		result = coinChangeNumberOfWaysBackTrackingOrderMatter(new int[] { 1, 2, 3 }, 4);		
+		System.out.println("coinChange number of ways backtrackingn order matter [1,2,3], 4 : " + result);
+*/		
 	}
 }
