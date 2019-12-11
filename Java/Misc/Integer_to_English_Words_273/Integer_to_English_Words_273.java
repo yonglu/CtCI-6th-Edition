caec6b252e50b3b795eabcd89e9aa71603d44b72
yonglu <ylu007@gmail.com>
2019-12-10 20:20:58 -0800

    a better solution

--- Java/Misc/Integer_to_English_Words_273/Integer_to_English_Words_273.java
+++ Java/Misc/Integer_to_English_Words_273/Integer_to_English_Words_273.java
@@ -32,7 +32,38 @@ Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Th
 
 public class Integer_to_English_Words_273 {
 
-    public static String numberToWords(int num) {
+	private static final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+	private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+	private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+	public static String numberToWordsBetter(int num) {
+	    if (num == 0) return "Zero";
+
+	    int i = 0;
+	    String words = "";
+	    
+	    while (num > 0) {
+	        if (num % 1000 != 0)
+	    	    words = helper(num % 1000) +THOUSANDS[i] + " " + words;
+	    	num /= 1000;
+	    	i++;
+	    }
+	    
+	    return words.trim();
+	}
+
+	private static String helper(int num) {
+	    if (num == 0)
+	        return "";
+	    else if (num < 20)
+	        return LESS_THAN_20[num] + " ";
+	    else if (num < 100)
+	        return TENS[num / 10] + " " + helper(num % 10);
+	    else
+	        return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+	}	
+
+	public static String numberToWords(int num) {
     	StringBuilder sb = new StringBuilder();
     	if (num < 0) {
     		sb.append("Negative ");
@@ -229,5 +260,9 @@ public class Integer_to_English_Words_273 {
 
 		result = numberToWords(60000001);		
 		System.out.println("60000001 is : " + result);	
+		
+		result = numberToWordsBetter(60000001);		
+		System.out.println("60000001 is : " + result);	
 	}
+	
 }
