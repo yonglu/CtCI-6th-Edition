1edd620a38c19e33c16059154ea50da90e8ce4c4
Yong Lu <y.lu@f5.com>
2019-12-17 16:16:20 -0800

    Solve it

--- Java/Two Pointers/Container_With_Most_Water_11/Containers_With_Most_Water_11.java
+++ Java/Two Pointers/Container_With_Most_Water_11/Containers_With_Most_Water_11.java
@@ -22,7 +22,15 @@ Output: 49
 
 public class Containers_With_Most_Water_11 {
 
-	
+	/*
+	 * The area formed between the lines will always be limited by the height of the shorter line. 
+	 * Further, the farther the lines, the more will be the area obtained.
+	 * 
+	 * We take two pointers, one at the beginning and one at the end of the array constituting 
+	 * the length of the lines. Further, we maintain a variable areaMax to store the maximum 
+	 * area obtained till now. At every step, we find out the area formed between them, 
+	 * update areaMax and move the pointer pointing to the shorter line towards the other end by one step.
+	 */
 	public static int maxArea(int[] height) {
 		int areaMax = 0;
 		if (height == null || height.length == 0) {
